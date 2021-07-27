97d54aceabff41a589c464e8ebb5ba1dd4852fe7
Joey_M_Brown <joey.ma.brown@gmail.com>
2021-07-27 19:50:30 -0400

    Built out individual plant component, and nested plantimg component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Plant from './components/Plant';
 
 function App() {
+
+  //Plant route will change to dynamic route based on user, and plant id.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Switch>
+
+        <Route path='/plant'>
+          <Plant />
+        </Route>
+
+      </Switch>
     </div>
   );
 }
