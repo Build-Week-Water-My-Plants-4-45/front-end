3c9afa30ed4de55926453f3b20bfa70e97bfd4b5
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-07-29 12:07:22 -0400

    fixing errors

--- front-end/src/components/CreatePlant/CreatePlant.js
+++ front-end/src/components/CreatePlant/CreatePlant.js
@@ -1,94 +1,63 @@
-import React, { useState } from 'react';
-import { axiosWithAuth } from '../authorization/axiosWithAuth.js';
-import styled from "styled-components";
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  width: 800px;
-  margin: 0 auto;
-  align-items: center;
-  label {
-    margin-top: 2rem;
-    font-size: 2rem;
-  }
-  input {
-    width: 30rem;
-    padding: 1rem 0.25rem;
-    border-radius: 0.5rem;
-    font-size: 2rem;
-  }
-`
+const initialPlant = {
+    nickname: '',
+    species: '',
+    h2o_frequency: '',
+    image: '',
+};
 
-const SubmitButton = styled.button`
-  width: 20rem;
-  border: none;
-  padding: 1.5rem 0.5rem;
-  border-radius: 0.5rem;
-  margin-top: 2rem;
-  transition: all, 300ms ease-in;
-  &:hover {
-    background: rgba(43, 48, 58, 0.9);
-    transition: all, 300ms ease-in;
-    color: white;
-  }
-  &:active {
-    background: orange;
-  }
-`
+export const CreatePlant = (props) => {
+    const [plant, setPlant ] = useState(initialPlant);
+    const history = useHistory();
 
-export default function  CreatePlant(props) {
-  const [plant, setPlant] = useState({ nickName: "", species: "", h2oFrequency: "", image: "" });
 
-  const create = e => {
-    e.preventDefault();
-    axiosWithAuth()
-      .post("dummydata", plant)
-      .then(res => props.history.push('/plants'))
-      .catch(err => console.log(err));
-  }
+    const handleChange = (event) => {
+        setPlant({...plant, [event.target.name]: event.target.value});
+    }
 
-  const handleChange = e => {
-    setPlant({
-      ...plant,
-      [e.target.name]: e.target.value,
-    })
-  }
-  console.log(plant)
-  return (
-    <Form onSubmit={e => create(e)}>
-      <h2>Create your plant!</h2>
-      <label>Name </label>
-      <input
-        type="text"
-        name="nickName"
-        value={plant.nickName}
-        onChange={handleChange}
-      />
-      <label>Species </label>
-      <input
-        type="text"
-        name="species"
-        value={plant.species}
-        onChange={handleChange}
-      />
-      <label>H20frequency </label>
-      <input
-        type="number"
-        name="h2oFrequency"
-        value={plant.h2oFrequency}
-        onChange={handleChange}
-      />
-      <label>Image </label>
-      <input
-        type="text"
-        name="image"
-        value={plant.image}
-        onChange={handleChange}
-      />
-      <SubmitButton>Create Plant</SubmitButton>
-    </Form>
-  )
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.createPlant(plant);
+        history.push('/plants');
+    }
+
+    return (
+        <div>
+            <h1>Create Plant</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Nickname
+                    <input 
+                        type="text"
+                        name="nickname"
+                        value={plant.nickname}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <lable>
+                    H2o Frequency
+                    <input 
+                        type="text"
+                        name="h20_frequency"
+                        value={plant.h2o_frequency}
+                        onChange={handleChange}
+                    />
+                </lable>
+
+                <label>
+                    Image
+                    <input 
+                        type="text"
+                        name="image"
+                        value={plant.image}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
-
-// export default CreatePlant;
