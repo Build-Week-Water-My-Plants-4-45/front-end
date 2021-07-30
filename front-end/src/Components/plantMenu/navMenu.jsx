62253273b565f21f45cf75b5526928585297cf67
Joey_M_Brown <joey.ma.brown@gmail.com>
2021-07-30 16:04:11 -0400

    Changed Logout back to logout on nav menu

--- front-end/src/Components/plantMenu/navMenu.jsx
+++ front-end/src/Components/plantMenu/navMenu.jsx
@@ -146,7 +146,7 @@ export function NavMenu({ isOpen }) {
             },
           }}
         >
-          <Link to={`/`}>CONTACT</Link>
+          <Link to={`/`}>LOGOUT</Link>
         </NavLink>
       </NavList>
     </NavMenuContainer>
